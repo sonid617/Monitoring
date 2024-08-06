3497e0b0600887a7ff5a034e5b4d1e94b0f4ab29
sonid617 <148606633+sonid617@users.noreply.github.com>
2024-08-06 21:28:48 +0530

    Delete server_monitor directory

--- server_monitor/app/routes.py
+++
@@ -1,9 +0,0 @@
-from flask import render_template
-from app import app
-from app.monitoring import get_system_metrics, get_disk_usage
-
-@app.route('/')
-def index():
-    cpu, memory = get_system_metrics()
-    disk = get_disk_usage()
-    return render_template('index.html', cpu=cpu, memory=memory, disk=disk)
